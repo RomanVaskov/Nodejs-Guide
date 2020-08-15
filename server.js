6652829624fb117ecc6da422d376432462c52897
Igor <aio350@yahoo.com>
2020-08-15 13:27:17 +0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
-const app = require('express')()
-
-app.use((req, res) => {
-    res.setHeader('Accept-Encoding', 'gzip, compress, br')
-    res.sendFile(__dirname + decodeURIComponent(req.url))
-})
-
-app.listen(process.env.PORT || 3000)
+const app=require("express")();app.use((req,res)=>{res.setHeader("Accept-Encoding","gzip, compress, br"),res.sendFile(__dirname+decodeURIComponent(req.url))}),app.listen(process.env.PORT||3e3);
